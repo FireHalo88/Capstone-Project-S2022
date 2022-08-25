5910e2be42ab0553d1468c0fea895b5a0c3160ec
tor <tor.w.lim@student.uts.edu.au>
2022-08-25 16:30:54 +1000

    Updated Image Capture 25/8

---
+++ src/image_capture/src/imageCapture.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sensor_msgs/fill_image.h>
+#include <sensor_msgs/point_cloud2_iterator.h>
+
+#include "opencv2/opencv.hpp"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <stdio.h>
+
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/image_encodings.h>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
+#include <std_msgs/String.h>
+#include <thread>
+
+#include <cv_bridge/cv_bridge.h>
+
+#include <time.h>
+
+
+int main()
+{
+ printf("Sup");
+ return 0;
+}
